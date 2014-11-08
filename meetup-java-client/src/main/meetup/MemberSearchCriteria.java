3c2e7b2c8f1e5ac66d625c945cd2d0ff88c9f2a0
SaifJerbi <m.jerbi.saif@gmail.com>
2014-11-08 11:58:49 +0100

    First release

---
+++ meetup-java-client/src/main/meetup/MemberSearchCriteria.java
@@ -0,0 +1,49 @@
+package meetup;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemberSearchCriteria {
+	private String member_id;
+	private String group_id;
+	private String group_urlname;
+
+	public Map<String, String> getParameterMap() {
+
+		Map<String, String> m = new HashMap<String, String>();
+		if (getMember_id() != null)
+			m.put("member_id", getMember_id());
+		if (getGroup_id() != null)
+			m.put("group_id", getGroup_id());
+		if (getGroup_urlname() != null)
+			m.put("group_urlname", getGroup_urlname());
+
+		return m;
+
+	}
+
+	public String getMember_id() {
+		return member_id;
+	}
+
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+
+	public String getGroup_id() {
+		return group_id;
+	}
+
+	public void setGroup_id(String group_id) {
+		this.group_id = group_id;
+	}
+
+	public String getGroup_urlname() {
+		return group_urlname;
+	}
+
+	public void setGroup_urlname(String group_urlname) {
+		this.group_urlname = group_urlname;
+	}
+
+}
