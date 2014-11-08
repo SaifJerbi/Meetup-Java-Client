060b4b20fa5facfdf6131217baf1c388e1887d42
SaifJerbi <m.jerbi.saif@gmail.com>
2014-11-08 13:59:12 +0100

    POM remote repository

--- meetup-java-client/src/main/meetup/MeetupClient.java
+++ meetup-java-client/src/main/meetup/MeetupClient.java
@@ -666,10 +666,8 @@ public class MeetupClient {
 						+ EntityUtils.toString(rsp.getEntity()));
 			}
 		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
